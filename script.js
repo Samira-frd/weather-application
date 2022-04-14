a8b958ace654aae00b0c15bca5ee3bff5879426f
Samira Farokhnejad <samira.frd@hotmail.com>
2022-04-14 11:19:33 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// http://api.openweathermap.org/geo/1.0/direct?q=Sydney,ISO,2077&appid=5d71cab5e4061ca0bcebeed08cdbecd9&units=metric
+
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+
+const apiKey = "5d71cab5e4061ca0bcebeed08cdbecd9";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let inputVal = input.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markUp = `<h2 class='city-name' data-name=${name},${sys.country}>
+        <span>${name}</span>
+        <span>${sys.country}</span>
+        </h2>
+        <div class='city-temp'>${Math.round(main.temp)}</div>
+        <figure>
+            <img class='city-icon' src='${icon}' alt='city'>
+            <figurecaption>${weather[0]["description"]}</figurecaption>
+        </figure>
+        `;
+        li.innerHTML = markUp;
+        list.appendChild(li);
+        msg.innerHTML = "";
+    })
+    .catch(()=>{
+        msg.innerHTML = "Please search for a valid city."
+    });
+    input.value = "";
+});
